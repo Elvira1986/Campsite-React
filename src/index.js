82124f26dbdfd4575e7aed0dd9c7c80c0c5662bf
Elvira Gizatullina <elvirawed@gmail.com>
2022-06-22 15:16:53 -0600

    Router commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
-
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-social/bootstrap-social.css";
 import "font-awesome/css/font-awesome.css";
 import "typeface-lobster";
 import "typeface-open-sans";
-import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -17,7 +17,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
